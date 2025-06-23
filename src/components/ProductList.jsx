9e5c42232ab7d7ca5f8c6504993fd2bf70dd24f5
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-06-23 21:05:25 +0600

    add product by axios and rtk

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const retrieveProducts = async ({queryKey}) => {
+  
+  const response = await axios.get(`http://localhost:8000/${queryKey[0]}`);
+
+  return response.data;
+};
+
+export default function ProductList() {
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: retrieveProducts,
+    retry: false,
+  });
+  console.log(products);
+  if (isLoading) return <div>Fetching products.....</div>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <div className="flex flex-col justify-center items-center w-3/5">
+      <h2 className="text-3xl my-2">Product List</h2>
+      <ul className="flex flex-wrap justify-center items-center">
+        {products &&
+          products.map((product) => (
+            <li
+              key={product.id}
+              className="flex flex-col items-center m-2 border rounded-sm"
+            >
+              <img
+                className="object-cover h-64 w-96 rounded-sm"
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <p className="text-xl my-3">{product.title}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
